0b3e4401150c4492dbcbe09d0f64005b15c8cbe9
Benjamin Hartmann <hbenjamin@tutanota.de>
2022-07-13 08:49:51 +0200

    Fehlermeldungen in Alertbox anzeigen

--- resources/js/Components/ValidationErrors.vue
+++ resources/js/Components/ValidationErrors.vue
@@ -8,11 +8,11 @@ const hasErrors = computed(() => Object.keys(errors.value).length > 0);
 </script>
 
 <template>
-    <div v-if="hasErrors">
-        <div class="font-medium text-red-600">Whoops! Something went wrong.</div>
+    <div v-if="hasErrors" class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800">
+        <div class="font-medium text-red-600">Ups! Da ist wohl etwas schief gegangen.</div>
 
         <ul class="mt-3 list-disc list-inside text-sm text-red-600">
-            <li v-for="(error, key) in errors" :key="key">{{ error }}</li>
+            <p v-for="(error, key) in errors" :key="key">{{ error }}</p>
         </ul>
     </div>
 </template>
