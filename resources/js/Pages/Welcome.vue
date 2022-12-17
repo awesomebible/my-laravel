481cee9f183ab75e113b39911da03a81431f64be
Benjamin <benjamin@awesomebible.de>
2022-12-17 13:50:33 +0100

    ✍️ Branding aktualisiert

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -27,7 +27,7 @@ import Footer from '../Components/Footer.vue';
             </div>
         </nav>
         <div class="container max-w-lg px-4 py-32 mx-auto text-left md:max-w-none md:text-center" id="">
-            <h1 class="text-5xl font-extrabold tracking-tight text-left text-gray-900 leading-tightest md:leading-10 md:text-center sm:leading-none md:text-6xl lg:text-7xl"><span class="inline md:block" id="">Die Bibel ist ein Buch...</span> <span class="relative mt-2 text-transparent md:inline-block bg-clip-text bg-gradient-to-br from-purple-600 to-blue-500" id="">tu was damit!</span></h1>
+            <h1 class="text-5xl font-extrabold tracking-tight text-left text-gray-900 leading-tightest md:leading-10 md:text-center sm:leading-none md:text-6xl lg:text-7xl"><span class="inline md:block" id="">Deine Bibel</span> <span class="relative mt-2 text-transparent md:inline-block bg-clip-text bg-gradient-to-br from-purple-600 to-blue-500" id="">bekommt ein Upgrade!</span></h1>
             <div class="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg md:text-center lg:text-lg" id="">Wir haben my.awesomeBible gebaut, damit du die Bibel auf deinem Level erleben kannst.</div>
             <div class="flex flex-col items-center mt-12 text-center">
                 <span class="relative inline-flex w-full rounded-full shadow-sm md:w-auto">
